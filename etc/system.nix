52867c6ba08e108be1523530674981933712ee84
DoubleDotStudios <doubledotgames@gmail.com>
2025-10-19 18:31:13 +1300

    add system config

---
+++ etc/system.nix
@@ -0,0 +1,115 @@
+{ config, pkgs, ... }:
+
+{
+  # Bootloader.
+  boot.loader.limine = {
+    enable = true;
+    efiSupport = true;
+    maxGenerations = 5;
+    style = {
+      wallpapers = [];
+      graphicalTerminal = {
+        palette = "1e1e2e;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4";
+        brightPalette = "585b70;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4";
+        background = "1e1e2e";
+        foreground = "cdd6f4";
+        brightBackground = "585b70";
+        brightForeground = "cdd6f4";
+      };
+    };
+    extraConfig = ''
+    interface_branding:
+    '';
+  };
+
+  networking.networkmanager.enable = true;
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    # Applications
+    neovim
+    librewolf
+
+    # Utilities
+    xh
+    git
+    pnpm
+    nodejs_24
+
+    # Fonts
+    nerd-fonts.caskaydia-cove
+    nerd-fonts.jetbrains-mono
+
+    # WM Config
+    kitty-themes
+  ];
+
+  programs.hyprland.enable = true;
+  programs.firefox = {
+    enable = true;
+    package = pkgs.librewolf;
+  };
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
+
