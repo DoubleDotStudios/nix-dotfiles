aba41cb33a84f963aee8e943ad1bd876e36ad65d
DoubleDotStudios <doubledotgames@gmail.com>
2025-10-23 08:00:09 +1300

    update home-manager flake description

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "vaelixd home-manager configuration";
+  description = "onix home-manager configuration";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
